28f213db90c95116183a97d5ca2d9c9674daed8e
Mario Mezquida M <65036484+MarioMezquidaM@users.noreply.github.com>
2020-08-21 12:42:16 -0500

    firts-1

--- Assets/BarraProgr.cs
+++ Assets/BarraProgr.cs
@@ -1,74 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BarraProgr : MonoBehaviour {
 
+    public float velParrafo;
     public GameObject imagenCarga;
-    public Slider Barra;
+    public GameObject boton;
+    public GameObject Barraa;
+    Slider Barra;
 
     private AsyncOperation asyn;
 
-    //public float max;
-    //public float act;
-    //public Text valorString;
-    //int cont = 0;
-
-    //void Awake()
-    //{
-    //    Barra = GetComponent<Slider>();
-    //}
-
     public void ClickCarga(int nivel)
     {
-        imagenCarga.SetActive(true);
+
         StartCoroutine(Loadlevelslider(nivel));
+
         
-        //funcionPrueba();
     }
 
     IEnumerator Loadlevelslider(int nivel)
     {
         asyn = Application.LoadLevelAsync(nivel);
+
+        
         while (!asyn.isDone)
         {
+            //float progress = Mathf.Clamp01(asyn.progress / -9.9f);
             Barra.value = asyn.progress;
-            yield return null;
-            //yield return new WaitForSeconds(5.1f);
+
+            //yield return null;
+            yield return new WaitForSeconds(velParrafo);
+        }
+
+    }
+
+    void Start()
+    {
+        boton.SetActive(true);
+        //funcionPrueba();
+        imagenCarga.SetActive(true);
+        Barraa.SetActive(true);
+    }
+
+    void Update()
+    {
+        //boton.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fill"))
+        {
+            boton.SetActive(true);
         }
     }
 
-    //void Start()
-    //{
-
-    //    funcionPrueba();
-    //}
-
-    //void Update()
-    //{
-    //    //ActualizarValorBarra(max, act);
-    //}
-
-    //void ActualizarValorBarra(float valorMax, float valorAct)
-    //{
-    //    float porcentaje;
-    //    porcentaje = valorAct / valorMax;
-    //    Barra.value = porcentaje;
-    //    valorString.text = porcentaje * 100 + "%";
-    //}
-
-    //void funcionPrueba()
-    //{
-    //    if (cont <= 1)
-    //    {
-    //        ActualizarValorBarra(1, cont);
-    //        cont++;
-    //    }
-    //    else
-    //    {
-    //        cont = 0;
-    //    }
-    //    Invoke("funcionPrueba", 5.0f);
-    //}
 }
